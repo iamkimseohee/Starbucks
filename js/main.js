d4d7a1a3304c70621ec56e8ee6ce6e957350cd7b
iamkimseohee <kimd5ri@gmail.com>
2023-12-07 11:49:47 +0900

    231207

--- js/main.js
+++ js/main.js
@@ -14,3 +14,27 @@ searchInputEl.addEventListener("blur", function () {
   searchEl.classList.remove("focused");
   searchInputEl.setAttribute("placeholder", "");
 });
+
+const badgeEl = document.querySelector("header .badges");
+window.addEventListener(
+  "scroll",
+  _.throttle(function () {
+    console.log(window.scrollY);
+    if (window.scrollY > 500) {
+      // 배지 숨기기
+      // badgeEl.style.display = "none";
+      // gsap.to(요소, 지속시간, 옵션) -> 초단위
+      gsap.to(badgeEl, 0.6, {
+        opacity: 0,
+        display: "none",
+      });
+    } else {
+      //배지 보이기
+      // badgeEl.style.display = "block";
+      gsap.to(badgeEl, 0.6, {
+        opacity: 1,
+        display: "block",
+      });
+    }
+  }, 300)
+);
